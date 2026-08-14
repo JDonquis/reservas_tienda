e2861528ce48da9a89624d5212ba4dc9aecc64c9
Juan Donquis <juandonquis07@gmail.com>
2026-08-14 13:05:26 -0400

    auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -15,12 +15,7 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-        return response()->json([
-            'status' => 'success',
-        ]);
-
-
-        $credentials = $request->validate([
+              $credentials = $request->validate([
             'email' => 'required|email',
             'password' => 'required',
         ]);
